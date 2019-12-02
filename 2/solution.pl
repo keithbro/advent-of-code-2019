d3cf27af0af785122d375dccd6294e0c68f631cb
Keith Broughton <keithbro256@gmail.com>
2019-12-02 20:14:29 +1100

    Solve Day 2 Part 1

--- 2/solution.pl
+++ 2/solution.pl
@@ -9,24 +9,38 @@ my $filename = 'input.txt';
 open(my $fh, '<:encoding(UTF-8)', $filename) or die $!;
 
 my $row = <$fh>;
-print "Input is: $row";
 
 my @integers = split /,/, $row;
-my $opcode = $integers[0];
+$integers[1] = 12;
+$integers[2] = 2;
 
-given ($opcode) {
-    when("1") {
-        my $x = $integers[$integers[1]];
-        my $y = $integers[$integers[2]];
-        my $z = $x + $y;
-        my $dest = $integers[$integers[3]];
-        say "Adding $x + $y (= $z) and storing it at position $dest...";
-        $integers[$dest] = $z;
-        say "Done."
-    }
-    default {
-        die "Unhandled opcode ($opcode)";
-    }
+print "Input is: ". join ',', @integers;
+
+my $opcode_idx = 0;
+my $opcode = "";
+while($opcode_idx < scalar @integers && $opcode ne "99") {
+    ($opcode_idx, $opcode) = evolve($opcode_idx, \@integers);
+    say "Output is: " . join ',', @integers;
 }
 
-say "Output is: " . join ',', @integers;
+sub evolve {
+    my ($opcode_idx, $integers) = @_;
+    say "opcode_idx: $opcode_idx";
+
+    my $opcode = $integers->[$opcode_idx];
+    say "opcode: $opcode";
+    return $opcode_idx, $opcode if $opcode eq "99";
+
+    my $x = $integers->[$integers->[$opcode_idx + 1]];
+    my $y = $integers->[$integers->[$opcode_idx + 2]];
+    my $dest_idx = $integers->[$opcode_idx + 3];
+
+    my $z = {
+        "1"  => sub { $x + $y },
+        "2"  => sub { $x * $y },
+    }->{$opcode}();
+
+    $integers->[$dest_idx] = $z;
+
+    return ($opcode_idx + 4, $opcode);
+}
