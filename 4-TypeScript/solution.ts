f494824b633b9f9fd495130ff5e75bc8c72962f8
Keith Broughton <keithbro256@gmail.com>
2019-12-05 13:57:10 +1100

    Solved 4:2

--- 4-TypeScript/solution.ts
+++ 4-TypeScript/solution.ts
@@ -1,8 +1,26 @@
 import * as assert from 'assert';
 
-const areTwoAdjacentNumbersTheSame = (i: number) : boolean => {
-  const str = i.toString();
-  return str.match(/(\d)\1/) !== null
+const countOccurrences = (acc: Map<string, number>, cur: string, idx: number, src: Array<string>): Map<string, number> => {
+  const prev : string | null = src[idx - 1];
+  if (prev === cur) { acc.set(cur, (acc.get(cur) || 1) + 1) }
+
+  return acc;
+}
+
+const areGroupsPresent = (i: number) : boolean => {
+  const digits = i.toString().split('');
+  const digitCounts = digits.reduce(countOccurrences, new Map());
+  const values = Array.from(digitCounts.values());
+
+  return values.some(x => x > 1);
+};
+
+const arePairsPresent = (i: number) : boolean => {
+  const digits = i.toString().split('');
+  const digitCounts = digits.reduce(countOccurrences, new Map());
+  const values = Array.from(digitCounts.values());
+
+  return values.some(x => x === 2);
 };
 
 const digitsIncreaseOrStayTheSame = (i: number) : boolean => {
@@ -17,31 +35,52 @@ const digitsIncreaseOrStayTheSame = (i: number) : boolean => {
   return true;
 };
 
-const countMatching = (min: number, max: number) : number => {
-  let count = 0;
+const countMatching = (min: number, max: number) : object => {
+  let counts = { one: 0, two: 0 };
 
   for (let i = min; i <= max; i++) {
-    if (areTwoAdjacentNumbersTheSame(i) && digitsIncreaseOrStayTheSame(i)) {
-      count++;
+    if (digitsIncreaseOrStayTheSame(i)) {
+      if (areGroupsPresent(i)) {
+        counts.one++;
+      }
+      if (arePairsPresent(i)) {
+        counts.two++;
+      }
     }
   }
 
-  return count;
+  return counts;
 };
 
 console.log("Tests");
-assert.strictEqual(areTwoAdjacentNumbersTheSame(123456), false);
-assert.strictEqual(areTwoAdjacentNumbersTheSame(122456), true);
+assert.strictEqual(arePairsPresent(123456), false);
+assert.strictEqual(arePairsPresent(122456), true);
+assert.strictEqual(arePairsPresent(112233), true);
+assert.strictEqual(arePairsPresent(123444), false);
+assert.strictEqual(arePairsPresent(111122), true);
+assert.strictEqual(arePairsPresent(123451), false);
+assert.strictEqual(arePairsPresent(111111), false);
+
+assert.strictEqual(areGroupsPresent(123456), false);
+assert.strictEqual(areGroupsPresent(122456), true);
+assert.strictEqual(areGroupsPresent(112233), true);
+assert.strictEqual(areGroupsPresent(123444), true);
+assert.strictEqual(areGroupsPresent(111122), true);
+assert.strictEqual(areGroupsPresent(123451), false);
+assert.strictEqual(areGroupsPresent(111111), true);
 
 assert.strictEqual(digitsIncreaseOrStayTheSame(123456), true);
 assert.strictEqual(digitsIncreaseOrStayTheSame(123455), true);
 assert.strictEqual(digitsIncreaseOrStayTheSame(123454), false);
+assert.strictEqual(digitsIncreaseOrStayTheSame(112233), true);
+assert.strictEqual(digitsIncreaseOrStayTheSame(123444), true);
+assert.strictEqual(digitsIncreaseOrStayTheSame(111122), true);
 
-assert.strictEqual(countMatching(100000, 100010), 0);
-assert.strictEqual(countMatching(111111, 111121), 9);
+assert.deepEqual(countMatching(100000, 100010), { one: 0, two: 0 });
+assert.deepEqual(countMatching(111111, 111121), { one: 9, two: 0 });
+assert.deepEqual(countMatching(123456, 234567), { one: 1022, two: 727 });
+assert.deepEqual(countMatching(130254, 678275), { one: 2090, two: 1419 });
 
 console.log("All tests have passed.");
 
 console.log(countMatching(130254, 678275));
-
-
