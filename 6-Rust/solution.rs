4c5266367e724cf84ef1e7de7abcccdca989b7d1
Keith Broughton <keithbro256@gmail.com>
2019-12-07 15:47:01 +1100

    Refactor

--- 6-Rust/solution.rs
+++ 6-Rust/solution.rs
@@ -9,19 +9,18 @@ struct Orbit {
 
 fn main() {
     let filename = "input.txt";
+
+    let orbits = parse(&filename);
+    println!("With text:\n{:?}", orbits);
+}
+
+fn parse(filename: &str) -> Vec<Orbit> {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let orbits = reader.lines().fold(vec![], |mut acc, result| {
+    reader.lines().map(|result| {
         let line = result.unwrap();
         let ids : Vec<String>  = line.split(')').map(|x| x.to_string()).collect();
-        let orbit = Orbit { satellite: ids[0].to_string(), parent: ids[1].to_string() };
-
-        //println!("{}", line);
-        acc.push(orbit);
-        acc
-        //acc.push(line.split(')'));
-    });
-
-    println!("With text:\n{:?}", orbits);
+        Orbit { satellite: ids[0].to_string(), parent: ids[1].to_string() }
+    }).collect()
 }
