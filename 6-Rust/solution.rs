ecd74c9c33a2f322626ad174cc2fc4b9ee8cdcab
Keith Broughton <keithbro256@gmail.com>
2019-12-07 16:51:47 +1100

    Wrong answer

--- 6-Rust/solution.rs
+++ 6-Rust/solution.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
@@ -10,17 +11,38 @@ struct Orbit {
 fn main() {
     let filename = "input.txt";
 
-    let orbits = parse(&filename);
-    println!("With text:\n{:?}", orbits);
+    let orbit_map = parse(&filename);
+    let orbit_counts = get_orbit_counts(orbit_map);
+    println!("With text:\n{:?}", orbit_counts);
 }
 
-fn parse(filename: &str) -> Vec<Orbit> {
+fn get_orbit_count(orbit_map: &HashMap<String, String>, body: &str) -> u32 {
+    let mut x = body;
+    let mut count = 0;
+
+    while orbit_map.contains_key(x) {
+        count += 1;
+        x = orbit_map.get(x).unwrap();
+    }
+
+    count
+}
+
+fn get_orbit_counts(orbit_map: HashMap<String, String>) -> u32 {
+    orbit_map.values().fold(0, |mut count, satellite| {
+        count += get_orbit_count(&orbit_map, &satellite);
+        count
+    })
+}
+
+fn parse(filename: &str) -> HashMap<String, String> {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    reader.lines().map(|result| {
+    reader.lines().fold(HashMap::new(), |mut acc, result| {
         let line = result.unwrap();
-        let ids : Vec<String>  = line.split(')').map(|x| x.to_string()).collect();
-        Orbit { satellite: ids[0].to_string(), parent: ids[1].to_string() }
-    }).collect()
+        let ids : Vec<String> = line.split(')').map(|x| x.to_string()).collect();
+        acc.insert(ids[1].to_string(), ids[0].to_string());
+        acc
+    })
 }
